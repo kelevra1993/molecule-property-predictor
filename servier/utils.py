334cccbf4fe24da35f7d6338fef23ab4bca954cd
Robert Kelevra <kelevra.robert@gmail.com>
2023-10-23 00:09:53 +0200

    [Utilities] - Add Confusion Matrix

--- servier/utils.py
+++ servier/utils.py
@@ -1,6 +1,13 @@
 import os
 import json
 import yaml
+import matplotlib
+
+matplotlib.use("Agg")
+import numpy as np
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
 
 
 def format_configuration_variables(config):
@@ -31,8 +38,7 @@ def format_configuration_variables(config):
         "num_iterations": config["training"]["settings"]["num_iterations"],
         "learning_rate": float(config["training"]["hyperparameters"]["learning_rate"]),
         "index_iteration": config["inference"]["index_iteration"],
-        "scaler": config["inference"]["scaler"],
-        "LOG_ERROR": config["logging"]["log_error"],
+        "scaler": config["inference"]["scaler"]
     }
     return project_configuration_variables
 
@@ -63,9 +69,7 @@ def safe_dump(training_parameters, destination):
     """
     try:
         with open(destination, "w") as fp:
-            json.dump(
-                training_parameters, fp, sort_keys=True, indent=4, separators=(",", ":")
-            )
+            json.dump(training_parameters, fp, sort_keys=True, indent=4, separators=(",", ":"))
     except KeyboardInterrupt:
         safe_dump(training_parameters, destination)
 
@@ -118,3 +122,229 @@ def print_bold(output):
     :return:
     """
     print("\033[1m" + output + "\033[0m")
+
+
+# Confusion Matrix Functions Still in progress
+def get_new_figure(figure_title, figure_size=None):
+    """
+     Initialize graphics
+    :param figure_title:
+    :param figure_size:
+    :return:
+    """
+    fig1 = plt.figure(figure_title, figure_size)
+    ax1 = fig1.gca()  # Get Current Axis
+    ax1.cla()  # clear existing plot
+    return fig1, ax1
+
+
+def configure_cell_text_and_colors(array_dataframe, line, column, CellText, facecolors, posi, font_size, total_values):
+    """
+    :param array_dataframe:
+    :param line:
+    :param column:
+    :param CellText:
+    :param facecolors:
+    :param posi:
+    :param font_size:
+    :return:
+    """
+    text_to_add = []
+    text_to_delete = []
+
+    cell_value = array_dataframe[line][column]
+    per = (float(cell_value) / total_values) * 100
+    curr_column = array_dataframe[:, column]
+    ccl = len(curr_column)
+
+    # last line  and/or last column
+    if (column == (ccl - 1)) or (line == (ccl - 1)):
+        # totals and percents
+        if cell_value != 0:
+            if (column == ccl - 1) and (line == ccl - 1):
+                tot_rig = 0
+                for i in range(array_dataframe.shape[0] - 1):
+                    tot_rig += array_dataframe[i][i]
+                per_ok = (float(tot_rig) / cell_value) * 100
+            elif column == ccl - 1:
+                tot_rig = array_dataframe[line][line]
+                per_ok = (float(tot_rig) / cell_value) * 100
+            elif line == ccl - 1:
+                tot_rig = array_dataframe[column][column]
+                per_ok = (float(tot_rig) / cell_value) * 100
+        else:
+            per_ok = 0
+
+        per_ok_s = ["%.2f%%" % (per_ok), "100%"][per_ok == 100]
+
+        # text to delete
+        text_to_delete.append(CellText)
+
+        # text to add
+        font_prop = matplotlib.font_manager.FontProperties(weight="bold", size=font_size)
+
+        text_kwargs = dict(
+            color=[0.7059, 0.7451, 0.9412, 1.0],
+            ha="center",
+            va="center",
+            gid="sum",
+            fontproperties=font_prop,
+        )
+
+        lis_txt = ["%d " % (cell_value), per_ok_s]
+        lis_kwa = [text_kwargs]
+
+        dic = text_kwargs.copy()
+        dic["color"] = "lawngreen"
+        lis_kwa.append(dic)
+
+        lis_pos = [(CellText._x, CellText._y - 0.11), (CellText._x, CellText._y + 0.11)]
+
+        for i in range(len(lis_txt)):
+            newText = dict(
+                x=lis_pos[i][0], y=lis_pos[i][1], text=lis_txt[i], kw=lis_kwa[i]
+            )
+            text_to_add.append(newText)
+
+        # set background color for sum cells (last line and last column)
+        carr = [0.0, 0.098, 0.21, 1.0]
+        if (column == ccl - 1) and (line == ccl - 1):
+            carr = [0.0, 0.098, 0.19, 1.0]
+        facecolors[posi] = carr
+
+    else:
+        txt = "%s \n  %.2f%%" % (cell_value, per)
+
+        CellText.set_text(txt)
+        CellText.set_color([0.0, 0.098, 0.25, 1.0])
+        CellText.set_weight("bold")
+
+        # main diagonal
+        if column == line:
+            # set background color in the diagonal to blue
+            CellText.set_color([0.0, 0.098, 0.19, 1.0])
+            facecolors[posi] = [0.4863, 0.988, 0.0, 1.0]
+
+    return text_to_add, text_to_delete
+
+
+def insert_totals(data_frame):
+    """
+    Functions that inserts totals on columns and lines
+    :param data_frame: (DataFrame)
+    :return:
+    """
+
+    sum_line = []
+    for item_line in data_frame.iterrows():
+        sum_line.append(item_line[1].sum())
+
+    sum_column = []
+    for column in data_frame.columns:
+        sum_column.append(data_frame[column].sum())
+
+    data_frame["Total \nPredictions \n Precision% "] = sum_line
+    sum_column.append(np.sum(sum_line))
+    data_frame.loc["Total Labels \nSensitivity%"] = sum_column
+
+
+def pretty_plot_confusion_matrix(data_frame, figure_size=None, model_iteration=None, iteration_result_folder=None):
+    """
+    Function that creates a fancy confusion matrix
+    :param data_frame: (DataFrame)
+    :param figure_size: List(int) size of the figure that is the confusion matrix
+    :param model_iteration: (int) iteration of the model that interests us
+    :param iteration_result_folder: (str) path to folder, where to store the information
+    :return:
+    """
+
+    x_label = "Labels"
+    y_label = "Predictions"
+
+    # Create "Total" Columns
+    insert_totals(data_frame)
+
+    # Default Confusion Matrix : Always print on same matrix
+    fig, ax1 = get_new_figure("Default Confusion Matrix", figure_size)
+
+    # thanks for seaborn
+    ax = sn.heatmap(data_frame, annot=True, annot_kws={"size": 11}, linewidths=2, ax=ax1,
+                    cbar=False, cmap="tab10_r", linecolor="w", fmt=".2f", )
+
+    # Set Y Tick labels Orientation
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0, fontsize=10, fontweight="bold")
+
+    # Bold Font For X labels
+    ax.set_xticklabels(ax.get_xticklabels(), fontweight="bold")
+
+    # Face Color List
+    quadmesh = ax.findobj(matplotlib.collections.QuadMesh)[0]
+    facecolors = quadmesh.get_facecolors()
+
+    # Iteration on text elements
+    array_dataframe = np.array(data_frame.to_records(index=False).tolist())
+    total_values = array_dataframe[-1][-1]
+    text_to_add = []
+    text_to_delete = []
+    position = -1  # from left to right, bottom to top.
+
+    for text in ax.collections[0].axes.texts:  # ax.texts:
+        pos = np.array(text.get_position()) - [0.5, 0.5]
+        line = int(pos[1])
+        column = int(pos[0])
+        position += 1
+
+        # set text
+        txt_results = configure_cell_text_and_colors(array_dataframe, line, column, text, facecolors, position, 11,
+                                                     total_values=total_values, )
+        text_to_add.extend(txt_results[0])
+        text_to_delete.extend(txt_results[1])
+
+    # Remove The Old Text
+    for item in text_to_delete:
+        item.remove()
+
+    # Append New Text
+    for item in text_to_add:
+        ax.text(item["x"], item["y"], item["text"], **item["kw"])
+
+    # Titles And Legends
+    ax.set_title("Iteration %d Confusion Matrix" % model_iteration, fontweight="bold")
+    ax.set_xlabel(x_label, fontweight="bold")
+
+    ax.xaxis.set_label_position("top")
+    ax.xaxis.set_ticks_position("top")
+
+    ax.set_ylabel(y_label, fontweight="bold")
+    plt.tight_layout()
+
+    # Save Figure in the Result Folder
+    plt.savefig(os.path.join(iteration_result_folder, "Confusion_Matrix_%d.png" % model_iteration))
+
+
+def plot_and_save_confusion_matrix(confusion_matrix, model_iteration, iteration_result_folder, num_classes,
+                                   label_dictionary):
+    """
+    # Confusion matrix imports
+    # plot a pretty confusion matrix with seaborn
+    # @author: Wagner Cipriano - wagnerbhbr - gmail - CEFETMG / MMC
+    # Link :https://github.com/wcipriano/pretty-print-confusion-matrix
+
+    Function that plots and saves the confusion matrix of a given iteration
+    :param confusion_matrix: np.array -> our confusion matrix
+    :param model_iteration: index / iteration of the model
+    :param iteration_result_folder: path to model's result folder
+    :return: save a png confusion matrix
+    """
+    # Get Pandas Data Frame
+    data_frame_confusion_matrix = pd.DataFrame(confusion_matrix, index=range(0, num_classes),
+                                               columns=range(0, num_classes))
+
+    data_frame_confusion_matrix = data_frame_confusion_matrix.rename(index=label_dictionary)
+    data_frame_confusion_matrix = data_frame_confusion_matrix.rename(columns=label_dictionary)
+
+    pretty_plot_confusion_matrix(
+        data_frame=data_frame_confusion_matrix,
+        figure_size=[num_classes + 5, num_classes + 5],
+        model_iteration=model_iteration,
+        iteration_result_folder=iteration_result_folder)
